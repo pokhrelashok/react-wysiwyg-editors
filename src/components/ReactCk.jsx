daedec2153d8f76b3428056ecf66717681a490f8
pokhrelashok2 <pokhrelashok2@gmail.com>
2023-05-16 20:53:17 +0545

    Custom colors added in ck & tiny

--- src/components/ReactCk.jsx
+++ src/components/ReactCk.jsx
@@ -1,10 +1,40 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from 'ckeditor5-essentials-build';
+const customColorPalette = [
+  {
+    color: 'white',
+    label: 'White'
+  },
+  {
+    color: 'black',
+    label: 'Black'
+  },
+  {
+    color: 'red',
+    label: 'Red'
+  },
+  {
+    color: 'blue',
+    label: 'Blue'
+  },
+  {
+    color: 'yellow',
+    label: 'Yellow'
+  },
+];
 
 const ReactCk = ({ value = "", onChange = () => { } }) => {
   return (<CKEditor
     editor={ClassicEditor}
     data={value}
+    config={{
+      fontColor: {
+        colors: customColorPalette
+      },
+      fontBackgroundColor: {
+        colors: customColorPalette
+      }
+    }}
     onChange={(event, editor) => {
       const data = editor.getData();
       onChange(data)
--- src/components/ReactTinyMce.jsx
+++ src/components/ReactTinyMce.jsx
@@ -22,12 +22,20 @@ function ReactTinyMce({ value = '', onChange = () => { } }) {
         plugins: [
           'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
           'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-          'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+          'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount', 'textcolor'
+        ],
+        color_map: [
+          "black", "Black",
+          "white", "White",
+          "red", "Red",
+          "green", "Green",
+          "blue", "Blue",
+
         ],
         toolbar: 'undo redo | blocks | ' +
           'bold italic forecolor fontfamily fontsize | alignleft aligncenter ' +
           'alignright alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | help',
+          'removeformat',
         fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
         branding: false,
         content_style: 'body { font-family:TImes New Roman,Arial,sans-serif; font-size:14px }',
